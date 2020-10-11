da65e93ad3d59da47292ce32216fb1cf87b16cbb
UVladislav <ustinowlad@gmail.com>
2020-10-11 19:48:09 +0300

    Изменённый Main

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -2,6 +2,14 @@ package Main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        // System.out.println("hello world");
+        Lesson1.calc(10.1f, 10.2f,10.3f,10.4f);
+        Lesson1.range(13,4);
+        Lesson1.posOrNeg(7);
+        Lesson1.antipositive(-6);
+        Lesson1.yourName("Влад");
+        System.out.println();
+
+        Lesson2.invMass();
     }
 }
